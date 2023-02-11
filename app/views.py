3b876887c60e9e961477e3925d77b9a85fda52d6
Patrick Witter <pwitterop123@gmail.com>
2023-02-10 22:50:24 -0500

    Initially Finished

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
+
+def format_date_joined(unformatted_date):
+    formatted_date = unformatted_date.strftime("%B, %Y")
+
+    return formatted_date
 
 ###
 # Routing for your application.
@@ -17,6 +23,19 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    name = "Mary Jane"
+    username = "mjane"
+    parish, country = "Kingston", "Jamaica"
+    bio = "I am smart and talented young woman who loves website design and development. Contact me if you'd like to work together on a new project."
+    num_posts, num_follows, num_followers = 7, 100, 250
+
+    
+    unformatted_date = datetime.datetime.now()
+    date = format_date_joined(unformatted_date)
+
+    return render_template('profile.html', name=name, username=username, parish=parish, country=country, date_joined=date, bio=bio, num_posts=num_posts, num_follows=num_follows, num_followers=num_followers)
 
 ###
 # The functions below should be applicable to all Flask apps.
